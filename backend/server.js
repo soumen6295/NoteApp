8a62d615376cb6498d09fb553b31f3193c66215a
soumen6295 <soumen.mondal6u@gmail.com>
2025-08-27 12:23:05 +0530

    Updated Notes App with localStorage persistence and Bootstrap styling

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,10 @@ dbConnection();
 
 app.use(express.json());
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'http://localhost:5174',
     credentials:true,
 }))
+
 
 app.use("/user",route);
 app.use("/todo",todoRoute);
